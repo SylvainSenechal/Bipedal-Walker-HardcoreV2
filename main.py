dc80497af0e967e43e716faa7f55513d6ecd3772
SylvainSenechal <senechalsylvain@hotmail.com>
2019-12-14 01:36:05 +0100

    added spider plotter

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from PairAgentEnv import PairAgentEnv
+from plotter import plotSpider
 # nb : install gym with : pip install 'gym[all]'
+
+
 
 ### CREATING AN ENVIRONNEMENT :
 # env = BipedalWalker(PIT, STUMP, HEIGHT)
@@ -17,12 +20,19 @@ thresholdTooHard = 50
 def POET():
     pass
 
+# def rawOptimization():
+data = [['PIT', 'STUMP', 'HEIGHT'],
+        ('Basecase', [
+            [0.2, 0.2, 0.8],
+            [0.4, 0.0, 0.5],
+            [0.6, 0.4, 0.9]])]
+plotSpider(data)
 def environnementDifficulty(difficultyPIT, difficultySTUMP, difficultyHEIGHT): # used for plotting latter
     return (difficultyPIT * 2 + difficultySTUMP * 2 + difficultyHEIGHT) / 5 # (normalized [0, 1], height count less than other)
 
 def main():
     pair = PairAgentEnv(difficultyPIT = 0, difficultySTUMP = 0, difficultyHEIGHT = 1)
-    pair.optimize()
+    # pair.optimize()
     # pair.benchmark()
     pair.saveBrain("brain1.txt")
     pair2 = PairAgentEnv(difficultyPIT = 0, difficultySTUMP = 0, difficultyHEIGHT = 1)
